f0aa5ab743e3fcec04a700bb7bc68f003735df12
gencay.sayin <gencay.sayin@keove.com>
2024-07-21 19:17:05 +0300

    server files updated

--- src/services/nlu/grpc/server.py
+++ src/services/nlu/grpc/server.py
@@ -1,34 +1,36 @@
-import grpc
 from concurrent import futures
-import nlu_pb2
-import nlu_pb2_grpc
+
+import grpc
+
+from src.services.nlu.grpc import nlu_pb2, nlu_pb2_grpc
+from src.services.nlu.grpc.nlu_pb2_grpc import NLUServiceServicer
 from src.services.nlu.nlu_processor import NLUProcessor
 
 
-class NLUService(nlu_pb2_grpc.NLUServiceServicer):
-
+class NLUService(NLUServiceServicer):
     def __init__(self):
         self.nlu_processor = NLUProcessor()
 
-    async def StreamAnalysis(self, request_iterator, context):
-        async for request in request_iterator:
-            sentiment, intention = await self.analyze(request.transcript)
-            yield nlu_pb2.NLUResponse(sentiment=sentiment, intention=intention)
+    def AnalyzeText(self, request_iterator, context):
+        for transcription_result in request_iterator:
+            sentiment_result = self.nlu_processor.get_sentiment(transcription_result.text)
+            intention_result = self.nlu_processor.get_intention(transcription_result.text)
 
-    async def analyze(self, transcript: str):
+            yield nlu_pb2.NLUResponse(
+                sentiment=sentiment_result,
+                intention=intention_result,
+                sequence_number=transcription_result.sequence_number
+            )
 
-        return self.nlu_processor.get_sentiment(transcript), self.nlu_processor.get_intention(transcript)
 
-
-async def serve():
-    server = grpc.aio.server(futures.ThreadPoolExecutor(max_workers=10))
-    nlu_pb2_grpc.add_NLUServiceServicer_to_server(NLUService(), server)
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    nlu_pb2_grpc.add_NLUServiceServicer_to_server(
+        NLUService(), server)
     server.add_insecure_port('[::]:50052')
-    await server.start()
-    await server.wait_for_termination()
+    server.start()
+    server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    import asyncio
-
-    asyncio.run(serve())
+    serve()
--- src/services/transcription/grpc/server.py
+++ src/services/transcription/grpc/server.py
@@ -1,32 +1,41 @@
-import grpc
 from concurrent import futures
 
-from src.services.transcription.grpc import transcription_pb2_grpc, transcription_pb2
+import grpc
+import numpy as np
+
+from src.services.common.models.sound_record import SoundRecord
+from src.services.transcription.grpc import transcription_pb2
+from src.services.transcription.grpc.transcription_pb2_grpc import TranscriptionServiceServicer, \
+    add_TranscriptionServiceServicer_to_server
 from src.services.transcription.transcription_processor import TranscriptionProcessor
 
 
-class TranscriptionService(transcription_pb2_grpc.TranscriptionServiceServicer):
+class TranscriptionService(TranscriptionServiceServicer):
+
     def __init__(self):
         self.transcription_processor = TranscriptionProcessor()
-    async def StreamTranscripts(self, request_iterator, context):
-        async for request in request_iterator:
-            transcript = await self.transcribe(request.audio_file_path)
-            yield transcription_pb2.TranscriptResponse(transcript=transcript)
 
-    async def transcribe(self, audio_file_path) -> str:
-        sr = self.transcription_processor.read_audio(audio_file_path)
-        return self.transcription_processor.audio_to_text(sr)
+    def StreamTranscription(self, request_iterator, context):
+        for audio_chunk in request_iterator:
+            audio_np = np.frombuffer(audio_chunk.audio_array, dtype=np.float32)
+
+            sr = SoundRecord(audio_array=audio_np, sample_rate=audio_chunk.sample_rate)
+            result = self.transcription_processor.audio_to_text(sr)
+
+            yield transcription_pb2.TranscriptionResult(
+                text=result,
+                sequence_number=audio_chunk.sequence_number
+            )
 
 
-async def serve():
-    server = grpc.aio.server(futures.ThreadPoolExecutor(max_workers=10))
-    transcription_pb2_grpc.add_TranscriptionServiceServicer_to_server(TranscriptionService(), server)
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    add_TranscriptionServiceServicer_to_server(
+        TranscriptionService(), server)
     server.add_insecure_port('[::]:50051')
-    await server.start()
-    await server.wait_for_termination()
+    server.start()
+    server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    import asyncio
-
-    asyncio.run(serve())
+    serve()
