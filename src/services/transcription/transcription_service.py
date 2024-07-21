5abe7d8b0e72c673988d3449e689d7d71fe1559a
gencay.sayin <gencay.sayin@keove.com>
2024-07-21 14:00:31 +0300

    SoundRecord object moved to common package

--- src/services/transcription/transcription_service.py
+++ src/services/transcription/transcription_service.py
@@ -1,6 +1,7 @@
+import librosa
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 
-from src.services.audio_generation.models.sound_record import SoundRecord
+from src.services.common.models.sound_record import SoundRecord
 
 
 class TranscriptionService:
@@ -9,7 +10,7 @@ class TranscriptionService:
         self.model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
         self.model.config.forced_decoder_ids = None
 
-    def audio_to_text(self, sound_record: SoundRecord):
+    def audio_to_text(self, sound_record: SoundRecord) -> str:
         if sound_record.sample_rate != 16000:
             raise ValueError("sample_rate must be 16000, provided {}.".format(sound_record.sample_rate))
 
@@ -18,3 +19,8 @@ class TranscriptionService:
 
         predicted_ids = self.model.generate(input_features)
         return self.processor.batch_decode(predicted_ids, skip_special_tokens=True)[0].strip()
+
+    def read_audio(self, file_name: str) -> SoundRecord:
+        audio_array, sample_rate = librosa.load(file_name, sr=16000)
+        return SoundRecord(audio_array=audio_array, sample_rate=sample_rate)
+
