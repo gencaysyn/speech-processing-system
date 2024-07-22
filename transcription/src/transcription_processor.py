e2c8f956be6fdb289359477d767740e61666ed4f
gencay.sayin <gencay.sayin@keove.com>
2024-07-22 11:03:44 +0300

    Revert "Revert "Feature/split services""
    This reverts commit 136402ca3274b0c13538b642f3978a950be9cea8.

--- transcription/src/transcription_processor.py
+++ transcription/src/transcription_processor.py
@@ -1,7 +1,7 @@
 import librosa
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 
-from src.services.common.models.sound_record import SoundRecord
+from models.sound_record import SoundRecord
 
 
 class TranscriptionProcessor:
